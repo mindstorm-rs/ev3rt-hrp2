3eadf9ba296ed51be3814cadf02b9c2bf8f7f352
luca <lucagiuggia@gmail.com>
2019-09-29 12:46:00 +0200

    Minor changes

--- x-build-env/run.sh
+++ x-build-env/run.sh
@@ -1,1 +1,1 @@
-docker run -t -i --rm -v `pwd`/..:/src:Z -w /src ev3dev-hrp2-xbuild:latest bash -c /src/x-build-env/build.sh
+docker run -t -i --rm -v `pwd`/..:/src:z -w /src ev3dev-hrp2-xbuild:latest bash -c /src/x-build-env/build.sh
