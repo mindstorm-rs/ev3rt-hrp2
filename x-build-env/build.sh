3e12959e3fc4ae28317669e2fa98a725433ed83c
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2024-11-03 17:23:33 +0100

    Also build bt examples.

--- x-build-env/build.sh
+++ x-build-env/build.sh
@@ -2,15 +2,37 @@
 cd /src/cfg
 make
 
-# Build loader uImage
+echo ---
+echo --- Building kernel and app loader
+echo ---
+make app=helloev3
+mv app hello-ev3
 cd /src/base-workspace
 make app=loader
 
-# Build helloev3 app
 cd /src/sdk/workspace
-make app=helloev3
-cp /src/sdk/workspace/app /src/sdk/workspace/helloev3
 
-# Build simple base app for Rust apps
+echo ---
+echo --- Building helloev3 app
+echo ---
+make app=helloev3
+mv app hello-ev3
+
+echo ---
+echo --- Building SPP master test app
+echo ---
+make app=test-spp-master
+mv app spp-master
+
+echo ---
+echo --- Building SPP slave test app
+echo ---
+make app=test-spp-slave
+mv app spp-slave
+
 cd /src/sdk/workspace
+echo ---
+echo --- Building simple base app for Rust apps
+echo ---
 make app=base-rs-app
+rm app
